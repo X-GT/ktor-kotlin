97a3d64ca657a97b3dc0de5be332c35f841b940a
Muchama <mafmudin@gamatechno.com>
2019-09-11 14:04:47 +0700

    use embeddedServer()

--- src/Application.kt
+++ src/Application.kt
@@ -1,44 +1,26 @@
 package dev.udinsi
 
 import com.fasterxml.jackson.databind.SerializationFeature
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.html.*
-import kotlinx.html.*
-import kotlinx.css.*
-import io.ktor.client.*
-import io.ktor.client.engine.apache.*
-import io.ktor.features.CallLogging
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
 import io.ktor.features.ContentNegotiation
-import io.ktor.features.DefaultHeaders
 import io.ktor.jackson.jackson
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import java.lang.Compiler.enable
 import java.util.*
 
-data class User(val text : String)
+data class User(val nama : String)
 
 val users = Collections.synchronizedList(mutableListOf(
     User("Udin"),
     User("Nopal")
 ))
 
-fun Application.main(){
-//    install(DefaultHeaders)
-//    install(CallLogging)
-//    install(Routing) {
-//        get("/") {
-//            call.respondText("My Example Blog2", ContentType.Text.Html)
-//        }
-//
-//        get("/udin") {
-//            call.respondText("ini udin", ContentType.Text.Html)
-//        }
-//    }
+fun Application.module(){
     install(ContentNegotiation) {
         jackson {
             enable(SerializationFeature.INDENT_OUTPUT) // Pretty Prints the JSON
@@ -51,20 +33,11 @@ fun Application.main(){
     }
 }
 
-//fun Application.module(){
-//    install(DefaultHeaders)
-//    install(CallLogging)
-//    install(Routing) {
-//        get("/") {
-//            call.respondText("My Example Blog2", ContentType.Text.Html)
-//        }
-//
-//        get("/udin"){
-//            call.respondText("ini udin", ContentType.Text.Html)
-//        }
-//    }
-//}
-
-//fun main(args: Array<String>) {
-//    embeddedServer(Netty, watchPaths = listOf(""), module = Application::module).start()
-//}
+fun main(args: Array<String>) {
+    embeddedServer(
+        Netty,
+        watchPaths = listOf("solutions/exercise4"),
+        port = 8080,
+        module = Application::module
+    ).apply { start(wait = true) }
+}
